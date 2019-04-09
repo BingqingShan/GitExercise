d0b50de3af93cc5efa51898740a0605acdf0cdeb
BingqingShan <bqshan@umich.edu>
2019-04-09 11:19:35 -0400

    add module and commements

--- prog.py
+++ prog.py
@@ -1,9 +1,10 @@
 import random
+import json
 
 
-def num_vowels(item):
-    vowel=0
-    for i in item:
-        if i in ["a", "e", "i", "o", "u"]:
-            vowel += 1
-    return vowel
+def num_vowels(item):#define a function
+    vowel=0 #set up the initial value of variable vowel
+    for i in item: #start a for loop
+        if i in ["a", "e", "i", "o", "u"]: #if the character is vowel
+            vowel += 1 #vowel is added up by 1
+    return vowel #return the value of vowel
